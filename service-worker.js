c895a12f2a7dabeba74367a0072efd164c77650d
mngsbe <56340589+mngsbe@users.noreply.github.com>
2019-10-10 23:05:53 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'zscore.be-v1.3.1';
+const CACHE_NAME = 'zscore.be-v1.3.2';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
