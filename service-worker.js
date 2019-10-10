e285127582fc4a3a01f5dbf2f3ee7f3d5ca503b7
mngsbe <56340589+mngsbe@users.noreply.github.com>
2019-10-11 01:35:01 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -19,22 +19,16 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('install', event => {
-//  self.skipWaiting();
-  console.log('[ServiceWorker] about to install');
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('[ServiceWorker] Caching page for offline use');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-  console.log('done...');
-
 });
 
 
 // Delete unused caches
 self.addEventListener('activate', event => {
-  console.log('[ServiceWorker] about to activate');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -48,26 +42,26 @@ self.addEventListener('activate', event => {
   );
 });
 
+ self.addEventListener('fetch', event => {
+   console.log('[ServiceWorker] will fetch');
+   event.respondWith(
+     caches.open(CACHE_NAME).then( cache =>
+     caches.match(event.request)).then( response => {
+       return response || fetch(event.request);
+     })
+   );
+ });
+
+
+
 // self.addEventListener('fetch', event => {
-//   console.log('[ServiceWorker] will fetch');
 //   event.respondWith(
-//     caches.open(CACHE_NAME).then( cache =>
-//     caches.match(event.request)).then( response => {
-//       return response || fetch(event.request);
-//     })
+//     caches.match(event.request).then( response => {
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
 //   );
 // });
-
-
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then( response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
