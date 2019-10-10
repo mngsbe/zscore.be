7151f672b6f398756c5cc1f45970c96a220cbc48
mngsbe <56340589+mngsbe@users.noreply.github.com>
2019-10-10 22:48:53 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -22,50 +22,37 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
-  console.log('[ServiceWorker] Install');
-  self.skipWaiting();
-  // CODELAB: Precache static resources here.
+//  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Caching page for offline use');
       return cache.addAll(FILES_TO_CACHE);
-    })
-  );    
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-  self.clients.claim();
- });
-
-self.addEventListener('fetch', (event) => {
-  console.log('[ServiceWorker] Fetch', event.request.url);
-  // CODELAB: Add fetch event handler here.
- event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      return response || fetchAndCache(event.request);
     })
   );
 });
 
-function fetchAndCache(url) {
-  return fetch(url)
-  .then(function(response) {
-    // Check if we received a valid response
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return caches.open(CACHE_NAME)
-    .then(function(cache) {
-      cache.put(url, response.clone());
-      return response;
-    });
-  })
-  .catch(function(error) {
-    console.log('Request failed:', error);
-    // You could return a custom offline 404 page here
-  });
-}
+
+// Delete unused caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
