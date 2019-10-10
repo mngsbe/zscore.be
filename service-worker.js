e5e08cc6aaed37053f84bb4df6bfbd7ff091d51a
mngsbe <56340589+mngsbe@users.noreply.github.com>
2019-10-10 23:47:12 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,21 +4,6 @@ let CACHE_NAME = 'zscore.be-v1.3.2';
 // CODELAB: Add list of files to cache here.
 let FILES_TO_CACHE = [
   '/',
-  'index.html',
-  'css/style.css',
-  'js/main.js',
-  'js/lmsfuns.js',
-  'js/install.js',
-  'images/logo/kuleuven.svg',
-  'images/logo/vwvj.svg',
-  'images/icons/icon-72x72.png',
-  'images/icons/icon-96x96.png',
-  'images/icons/icon-128x128.png',
-  'images/icons/icon-144x144.png',
-  'images/icons/icon-152x152.png',
-  'images/icons/icon-192x192.png',
-  'images/icons/icon-384x384.png',
-  'images/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
